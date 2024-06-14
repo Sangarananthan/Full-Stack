5dd2cda2190bebc650d8ee696cdfaac8f9d6e7c3
Sangar <sangarsmart16@gmail.com>
2024-06-14 09:55:29 +0530

    DONE

--- REACT/PROJECT/my-react-app/src/App.js
+++ REACT/PROJECT/my-react-app/src/App.js
@@ -1,5 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const [items, setitems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        setitems(res.data.meals);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <>
+      <header>
+        <h1>Sea Foods</h1>
+        <h1>🦐</h1>
+      </header>
+      <div className="card">
+        {items.map((item) => (
+          <div key={item.idMeal} className="carditems">
+            <img src={item.strMealThumb} alt={item.strMeal} />
+            <h1>{item.strMeal}</h1>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
