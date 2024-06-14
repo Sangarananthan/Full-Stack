1ee31ad8e1f8f8be10d1a11025bae110561c743d
Sangar <sangarsmart16@gmail.com>
2024-06-14 07:20:17 +0530

    done

--- REACT/PROJECT/TO-DO/src/ToDo.js
+++ REACT/PROJECT/TO-DO/src/ToDo.js
@@ -1,22 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./index.css";
+import { FaTrash } from "react-icons/fa";
+import { FaNoteSticky } from "react-icons/fa6";
 const ToDo = () => {
   const [value, setValue] = useState("");
-  const [tasks, settasks] = useState(["Hey"]);
+  const [tasks, setTasks] = useState([]);
+
+  const inputElment = useRef();
+
+  const FocusInput = () => {
+    inputElment.current.focus();
+  };
+
+  // Loading from local storage
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  // Updates local Storage
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // Code Logic
   const addTask = () => {
     if (value.trim()) {
-      settasks([...tasks, value.trim()]);
+      setTasks([...tasks, value.trim()]);
       setValue("");
     }
   };
 
-  //   Load Tasks on Refresh
-  useEffect(() => {
-    const taskScheduled = localStorage.getItem("tasks");
-    if (taskScheduled) {
-      settasks(taskScheduled);
-    }
-  }, []);
+  const removeTask = (id) => {
+    setTasks(tasks.filter((_, index) => index !== id));
+  };
 
   return (
     <>
@@ -28,20 +47,47 @@ const ToDo = () => {
               placeholder="Enter your task"
               value={value}
               onChange={(e) => setValue(e.target.value)}
-            ></input>
+              aria-label="Enter your task"
+              ref={inputElment}
+            />
             <button className="tsk-btn" onClick={addTask}>
               Add
             </button>
           </div>
           <div className="taskList">
-            {tasks.map((task) => (
-              <div className="list">
-                <input type="radio" className="radiobtn"></input>
-                <p>{task}</p>
-                <span>x</span>
-                <hr />
+            {tasks.length ? (
+              tasks.map((task, id) => (
+                <div className="list" key={id}>
+                  <input type="radio" className="radiobtn" />
+                  <p className="taskitem">{task}</p>
+                  <div className="trash" onClick={() => removeTask(id)}>
+                    <FaTrash />
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "100%",
+                  alignItems: "center",
+                }}
+                onClick={() => FocusInput()}
+              >
+                <h1
+                  style={{
+                    marginTop: "50px",
+                    fontFamily: "cursive",
+                  }}
+                >
+                  ADD TASKS
+                </h1>
+                <FaNoteSticky
+                  style={{ transform: "scale(7)", marginTop: "100px" }}
+                />
               </div>
-            ))}
+            )}
           </div>
         </div>
       </div>
