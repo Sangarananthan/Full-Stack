ad752aaefd348585acc5e007747076b1c483c107
Sangar <sangarsmart16@gmail.com>
2024-06-14 11:21:18 +0530

    Start

--- REACT/PROJECT/my-react-app/src/App.js
+++ REACT/PROJECT/my-react-app/src/App.js
@@ -1,37 +1,7 @@
-import { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [backgroundColor, setBackgroundColor] = useState("white");
-  const [textcolor, setTextcolor] = useState("#1b1b1b");
-  const [buttonStyle, setbuttonStyle] = useState("white");
-
-  const handlClick = () => {
-    setBackgroundColor(backgroundColor === "white" ? "#1b1b1b" : "white");
-    setTextcolor(textcolor === "#1b1b1b" ? "#ffa31a" : "#1b1b1b");
-    setbuttonStyle(buttonStyle === "white" ? "#1b1b1b" : "white");
-  };
-  return (
-    <>
-      <div
-        className="back"
-        style={{ backgroundColor, color: textcolor, transition: `all .5s` }}
-      >
-        <button
-          style={{
-            buttonStyle,
-            color: textcolor,
-            border: `3px solid ${textcolor}`,
-          }}
-          onClick={handlClick}
-        >
-          {backgroundColor === "white" ? "Dark Theme" : "Light Theme"}
-        </button>
-        <section className="content">
-          <h1>Welcome to real world 🤑</h1>
-        </section>
-      </div>
-    </>
-  );
+  return <div>App</div>;
 };
 
 export default App;
